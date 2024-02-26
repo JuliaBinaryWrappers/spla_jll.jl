14a95f410e50a60f063ab97fe9179f8c6b8a6fcf
jlbuild <juliabuildbot@gmail.com>
2024-02-26 17:38:50 +0000

    spla_jll build 1.5.5+0

--- src/spla_jll.jl
+++ src/spla_jll.jl
@@ -2,7 +2,9 @@
 baremodule spla_jll
 using Base
 using Base: UUID
+using LazyArtifacts
 using MPIPreferences
+Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("spla")
